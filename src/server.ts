a33f54cb3d56d50fbbc5a0fcecc9ead0d33f04b5
marcosgqb <marcos.gqb@gmail.com>
2021-02-16 02:29:47 -0300

    Adicionando varieis e procfile

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ app.get('/', (request, response)=>{
     return response.json({ message: 'Hello ' });
 });
 
-app.listen(3333, ()=>{
+app.listen(process.env.PORT||3333, ()=>{
     console.log('Server started on port 3333');
 });
 
